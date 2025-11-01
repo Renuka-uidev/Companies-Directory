fd21392ed11f0530c61bda3f34d1a3f06e246331
Renuka Thanniru <renuka.t@trysol.co.in>
2025-11-01 14:59:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import CompanyTable from './components/CompanyTable';
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Companies Directory</h1>
+     <CompanyTable />
+    </>
+  )
+}
+
+export default App
