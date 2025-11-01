acd8941fd7d8f6f84998f44f0ed8bf38ec94495d
Renuka Thanniru <renuka.t@trysol.co.in>
2025-11-01 15:50:21 +0530

    Update CompanyTable.jsx

--- src/components/CompanyTable.jsx
+++ src/components/CompanyTable.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Table,Form } from "react-bootstrap";
+import { Table, Form } from "react-bootstrap";
 import PaginationComponent from "./PaginationComponent";
 
 const CompanyTable = () => {
   const [companyData, setCompanyData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchCompany, setSearchCompany] = useState("");
-  const itemsPerPage = 5; 
+  const itemsPerPage = 5;
 
   useEffect(() => {
     fetch("/data.json")
@@ -16,30 +16,30 @@ const CompanyTable = () => {
       .catch((error) => console.log("Error Loading Data:", error));
   }, []);
 
+
   const filteredData = companyData.filter(
     (item) =>
       item.company_name.toLowerCase().includes(searchCompany.toLowerCase()) ||
       item.location.toLowerCase().includes(searchCompany.toLowerCase())
   );
 
-
-  const totalPages = Math.ceil(companyData.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
   const indexOfLast = currentPage * itemsPerPage;
   const indexOfFirst = indexOfLast - itemsPerPage;
- const currentItems = filteredData.slice(indexOfFirst, indexOfLast);
+  const currentItems = filteredData.slice(indexOfFirst, indexOfLast);
 
   const handlePageChange = (pageNumber) => setCurrentPage(pageNumber);
 
+  
   useEffect(() => {
     setCurrentPage(1);
   }, [searchCompany]);
-
- 
 
   return (
     <div className="container mt-4">
       <h4 className="text-center mb-3">Company List</h4>
 
+    
       <Form className="mb-3">
         <Form.Control
           type="text"
@@ -49,31 +49,43 @@ const CompanyTable = () => {
         />
       </Form>
 
-      <Table bordered hover responsive="sm" className="text-center">
-        <thead className="table-primary">
-          <tr>
-            <th>ID</th>
-            <th>Company Name</th>
-            <th>Location</th>
-            <th>Email</th>
-            <th>Industry Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentItems.map((c) => (
-            <tr key={c.id}>
-              <td>{c.id}</td>
-              <td>{c.company_name}</td>
-              <td>{c.location}</td>
-              <td>{c.email}</td>
-              <td>{c.industry_type}</td>
+    
+      <div className="table-responsive">
+        <Table bordered hover responsive className="text-center align-middle">
+          <thead className="table-primary">
+            <tr>
+              <th>ID</th>
+              <th>Company Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Industry Type</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {filteredData.length > 0 ? (
+              currentItems.map((c) => (
+                <tr key={c.id}>
+                  <td>{c.id}</td>
+                  <td>{c.company_name}</td>
+                  <td>{c.location}</td>
+                  <td>{c.email}</td>
+                  <td>{c.industry_type}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5" className="text-danger fw-semibold">
+                   There is no such company or location
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </div>
 
-  
-      <PaginationComponent  totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange} />
+      {filteredData.length > 0 && (
+        <PaginationComponent totalPages={totalPages}  currentPage={currentPage} onPageChange={handlePageChange}/>
+      )}
     </div>
   );
 };
